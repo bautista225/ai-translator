bfb50f65f1831ddbae1e428656de9c3c33265ed5
Juan Bautista García Traver <juangarciatraver@gmail.com>
2025-02-19 13:37:27 +0100

    Updates

---
+++ assets/loadSts-5955a16b.js
@@ -0,0 +1,1 @@
+import{A as s,S as t}from"./STS-00d90715.js";import"./index-20ffef33.js";import"./index-8445f478.js";export{s as AssumeRoleCommand,t as STSClient};
--- assets/loadSts-782f96d1.js
+++
@@ -1,1 +0,0 @@
-import{A as s,S as t}from"./STS-99b4063d.js";import"./index-336f687a.js";import"./index-7c8b6dcd.js";export{s as AssumeRoleCommand,t as STSClient};

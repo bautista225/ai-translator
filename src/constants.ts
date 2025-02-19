e956b6be60d10134548a2f623a74a457e12cdbbb
Juan Bautista García Traver <juangarciatraver@gmail.com>
2025-02-19 09:53:26 +0100

    Updates supported languages list

--- src/constants.ts
+++ src/constants.ts
@@ -2,8 +2,14 @@ export const SUPPORTED_LANGUAGES = {
   en: "English",
   es: "Spanish",
   fr: "French",
+  de: "German",
+  pt: "Portuguese",
+  it: "Italian",
+  zh: "Mandarin",
+  ko: "Korean",
+  ja: "Japanese",
+  ar: "Arabic",
 };
-//English, French, German, Spanish, Portuguese, Italian, Mandarin, Korean, Japanese, and Arabic.
 
 export const AUTO_LANGUAGE = "auto";
 
